98a2e1354dc676a1f86272ed1e97938ea837a483
maximopiccirillo <maximo.piccirillo@fing.edu.uy>
2026-04-26 19:55:10 -0300

    Wizard class modified  v1.0.1

--- src/Library/wizard.cs
+++ src/Library/wizard.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,9 @@ namespace Library
         public string Name{get;}
         public SpellsBook SpellsBook {get;}
         public Staff Staff{get;}
-        public int AttackValue{get;}
-        public int DefenseValue{get;}
-        public int Health{get;private set;}
+        public int AttackValue{get{ return SpellsBook.AttackValue + Staff.AttackValue;}}
+        public int DefenseValue{get{ return SpellsBook.DefenseValue + Staff.DefenseValue;}}
+        public int Health{get; private set;}
 
         public Wizard(string name)
         {
