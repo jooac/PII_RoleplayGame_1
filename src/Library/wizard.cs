5962017f237a5a3deff222f8fbc0357dcf0c8436
maximopiccirillo <maximo.piccirillo@fing.edu.uy>
2026-04-26 19:24:11 -0300

    Clase Wizard v1.0.0

--- src/Library/wizard.cs
+++ src/Library/wizard.cs
@@ -7,7 +7,31 @@ namespace Library
 {
     public class wizard
     {
-        
+        public string Name{get;}
+        public SpellsBook SpellsBook {get;}
+        public Staff Staff{get;}
+        public int AttackValue{get;}
+        public int DefenseValue{get;}
+        public int Health{get;private set;}
+
+        public Wizard(string name)
+        {
+            Name = name;
+            Health = 100;
+
+            SpellsBook = new SpellsBook();
+            Staff = new Staff();
+        }
+
+        public void ReceiveAttack(int power)
+        {
+            Health -= power;
+        }
+
+        public void Cure()
+        {
+            Health = 100;
+        }
         
     }
 }
